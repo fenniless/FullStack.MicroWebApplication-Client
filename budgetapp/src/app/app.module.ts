d68a194be3d367ec4000242cd1e5177c0eacf529
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro-2.local>
2019-04-14 16:54:01 -0400

    Transaction add buttons created

--- budgetapp/src/app/app.module.ts
+++ budgetapp/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-// import { TransactionComponent } from './transaction/transaction.component';
+import { TransactionComponent } from './transaction/transaction.component';
 // import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { UserSearchComponent } from './user-search/user-search.component';
@@ -18,7 +18,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [
     AppComponent,
     UserComponent,
-    // TransactionComponent,
+    TransactionComponent,
     // TransactionDetailComponent,
     MessagesComponent,
     UserSearchComponent,
