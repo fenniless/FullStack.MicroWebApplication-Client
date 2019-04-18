65c99cbb8ae228a045cb01bc980c6d4274a20b47
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-04-18 17:15:00 -0400

    Corrected transaction url

--- src/app/transaction.service.ts
+++ src/app/transaction.service.ts
@@ -24,14 +24,14 @@ export class TransactionService {
               private messageService: MessageService) {
   }
 
-  // private baseURI = 'https://budgetapp-server.herokuapp.com/budget/';
-  private baseURI = 'http://localhost:8080/budget/';
+  private baseURI = 'https://budgetapp-server.herokuapp.com/budget/';
+  // private baseURI = 'http://localhost:8080/budget/';
   private transactionUrl = `${this.baseURI}transaction/`;
   private accountUrl = `${this.baseURI}account`;
   private transactionTypeURL = `${this.baseURI}transactiontype`;
 
   private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
+    this.messageService.add(`TransactionService: ${message}`);
   }
 
   getTransactions(): Observable<Transaction[]> {
