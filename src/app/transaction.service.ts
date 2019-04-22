cff45ff05f5cb034e6b84de8855456bd10fd8392
Fenniless <fenniless@gmail.com>
2019-04-22 16:47:22 -0400

    swap urls

--- src/app/transaction.service.ts
+++ src/app/transaction.service.ts
@@ -24,8 +24,8 @@ export class TransactionService {
               private messageService: MessageService) {
   }
 
-   // private baseURI = 'https://budgetapp-server.herokuapp.com/budget/';
-  private baseURI = 'http://localhost:8080/budget/';
+   private baseURI = 'https://budgetapp-server.herokuapp.com/budget/';
+  // private baseURI = 'http://localhost:8080/budget/';
   private transactionUrl = `${this.baseURI}transaction/`;
   private accountUrl = `${this.baseURI}account`;
   private transactionTypeURL = `${this.baseURI}transactiontype`;
