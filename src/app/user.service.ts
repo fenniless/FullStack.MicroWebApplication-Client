5c415171e8214ea1f4a069873b44a62e90c85b09
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro-2.local>
2019-04-22 13:57:35 -0400

    merge fixes - dynamic loads

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,8 +9,8 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class UserService {
-  // private userUrl = 'https://budgetapp-server.herokuapp.com/budget/profile';
-  private userUrl = 'http://localhost:8080/budget/profile';
+   private userUrl = 'https://budgetapp-server.herokuapp.com/budget/profile';
+  //private userUrl = 'http://localhost:8080/budget/profile';
   private log(message: string) {
     this.messageService.add(`ProfileService: ${message}`);
   }
